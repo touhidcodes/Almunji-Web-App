5433ef4b283bc08cb3d56c9f8939cd97e528c329
touhidcodes <touhidcodes@gmail.com>
2025-06-23 22:08:50 +0600

    mod: layout page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["ui-sans-serif", "system-ui"],
+        mono: ["ui-monospace", "SFMono-Regular"],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
