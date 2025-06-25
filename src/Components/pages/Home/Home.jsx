2a76a69b2778b0c7cafe4d56f8056435320f5063
touhidcodes <touhidcodes@gmail.com>
2025-06-25 20:16:55 +0600

    mod: folder structure & redux

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import QuranLayout from "../../Components/QuranDisplayLayout/QuranLayout";
+import QuranLayout from "../../QuranDisplayLayout/QuranLayout";
 
 const Home = () => {
   return (
--- src/redux/tags.js
+++
@@ -1,7 +0,0 @@
-export const tagTypes = {
-  admin: "admin",
-  user: "user",
-  quran: "quran",
-};
-
-export const tagTypesList = [tagTypes.admin, tagTypes.user, tagTypes.quran];
