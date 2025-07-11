26720a36d38aeba5f962228886e6d28008b3d34d
touhidcodes <touhidcodes@gmail.com>
2025-07-11 22:23:47 +0600

    del: old navbar and footer, add: new navbar

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Shared/Navbar/Navbar";
 
 export const metadata: Metadata = {
   title: "Almunji",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
