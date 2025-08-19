c8c368d5343ee36902690d1e40dbb190b2ad7ddf
touhidcodes <touhidcodes@gmail.com>
2025-08-19 23:14:53 +0600

    updt: homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import DivineSection from "@/components/Pages/Homepage/DivineSection/DivineSection";
 import HeroSection from "@/components/Pages/Homepage/HeroSection/HeroSection";
+import IslamicLearningSection from "@/components/Pages/Homepage/IslamicLearningSection/IslamicLearningSection";
 import JourneySection from "@/components/Pages/Homepage/JourneySection/JourneySection";
 import Footer from "@/components/Shared/Footer/Footer";
 
@@ -8,6 +9,7 @@ const Homepage = () => {
     <div>
       <HeroSection />
       <DivineSection />
+      <IslamicLearningSection />
       <JourneySection />
       <Footer />
     </div>
