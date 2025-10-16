e950d5b9c34791df9dc5873928cb4f50abead8f2
touhidcodes <touhidcodes@gmail.com>
2025-10-16 23:10:29 +0600

    mod: next config optimized

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+  output: "standalone",
+  images: { unoptimized: true },
+  reactStrictMode: false,
+  compress: true,
 };
 
 export default nextConfig;
