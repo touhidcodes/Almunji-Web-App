12ede54e30ba09272b0ab2ed48b86a77b8cc05d5
touhidcodes <touhidcodes@gmail.com>
2025-10-18 22:21:45 +0600

    mod: production build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "sonner";
 import Providers from "@/providers/providers";
-import { Amiri } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Almunji Web App",
   description: "Almunji Web App",
 };
-
-const amiri = Amiri({
-  subsets: ["arabic", "latin"],
-  weight: ["400", "700"],
-  variable: "--font-amiri",
-});
 
 export default function RootLayout({
   children,
@@ -21,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={amiri.variable}>
+    <html lang="en">
       <body>
         <Toaster position="top-right" />
         <Providers>{children}</Providers>
